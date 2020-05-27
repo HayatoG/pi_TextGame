c0707c5b58aa5be60b3c668a1c17716facc0365a
HayatoG <>
2020-05-27 00:18:15 -0300

    Parte 4 - Atualizando função parte1 e variáveis faltando no JogoPI.

--- jogoPI/src/jogopi/JogoPI.java
+++ jogoPI/src/jogopi/JogoPI.java
@@ -6,10 +6,12 @@ public class JogoPI {
     static boolean Chaves=false;
     static boolean Foto=false;
     static boolean ChaveCarro=false;
+    static boolean gazua = false;
     static String lanternaEstante="L";
     static String chavesInvetario="K";
     static String fotoInvetario="F";
     static String chaveCarroInventario="C";
+    static String gazuaCaixas="Gazua";
     
     static  String [] inventario = new String [10];
     
