9a6201314b667ef513a4dd3204350ffba838721a
HayatoG <>
2020-05-26 22:43:50 -0300

    Parte 4 - Iniciando a função da parte 4

---
+++ jogoPI/src/jogopi/parte4.java
@@ -0,0 +1,11 @@
+package jogopi;
+import java.util.Scanner;
+
+public class parte4 {
+    static void parte4(){
+        //Colocar validação caso o usuário coloque String
+        Scanner s = new Scanner(System.in);
+        
+        corredorRat.corredor();
+    }
+}
