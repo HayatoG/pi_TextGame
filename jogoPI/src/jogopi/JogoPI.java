ffc0166491f7a9318ce34dab6d2d4d3761cb6e54
HayatoG <guilherme.ryders@gmail.com>
2020-05-27 00:29:07 -0300

    Master - Adicionando variáveis utilizadas na parte4 globais no JogoPI.

--- jogoPI/src/jogopi/JogoPI.java
+++ jogoPI/src/jogopi/JogoPI.java
@@ -3,11 +3,17 @@ package jogopi;
 public class JogoPI {
     
     static boolean Lanterna=false;
-    static String lanternaEstante="Lanterna";
+    static boolean Chaves=false;
+    static boolean Foto=false;
+    static boolean ChaveCarro=false;
     static boolean gazua = false;
+    static String lanternaEstante="L";
+    static String chavesInvetario="K";
+    static String fotoInvetario="F";
+    static String chaveCarroInventario="C";
     static String gazuaCaixas="Gazua";
-    static  String [] inventario = new String [10];
     
+    static  String [] inventario = new String [10];
     
     public static void main(String[] args) {
         telaInicial.telaInicio();
